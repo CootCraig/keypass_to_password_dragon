b72dd090ebd814e1a63fa7b542163c29d388d2a1
Craig Anderson <craig@coot.net>
2012-03-10 13:05:59 -0700

    Import to Password Dragon appears to be working

--- xlate.rb
+++ xlate.rb
@@ -27,16 +27,24 @@ def traverse_groups(parent_title,node)
       child.children.each do |child|
         if child.is_a?(Nokogiri::XML::Element) && child.name == 'entry'
           entry_title = child.css('title').first.content
-          notes = '"' + clean_notes(child.css('comment').first.inner_html) + '"'
+          notes = child.css('comment').first.inner_html
+          notes.strip!
+          if notes.length > 0
+            notes = '"' + clean_notes(notes) + '"'
+          end
           username = child.css('username').first.content
           password = child.css('password').first.content
+          password.strip!
+          if password.length > 0
+            password = '"' + clean_notes(password) + '"'
+          end
           url = child.css('url').first.content
 
           export = entry_title
           export += ",#{username}"
-          export += ",#{obfuscate('password')}"
+          export += ",#{password.encode('UTF-8')}"
           export += ",#{url}"
-          export += ",#{obfuscate('notes')}"
+          export += ",#{notes}"
           export += ",#{group_title}"
           puts export
         end
